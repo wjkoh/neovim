10051b88b6698e3e6ff7f5db6cf52fc0d34236ed
Woojong Koh <wjngkoh@gmail.com>
2026-01-24 14:11:10 +0900

    feat: add support for x86_64-linux

--- flake.nix
+++ flake.nix
@@ -4,15 +4,19 @@
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = {nixpkgs, ...} @ inputs: {
-    packages.aarch64-darwin = {
-      default =
-        (inputs.nvf.lib.neovimConfiguration {
-          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-          modules = [
-            ./configuration.nix
-          ];
-        }).neovim;
+  outputs = {nixpkgs, ...} @ inputs:
+    let
+      systems = ["aarch64-darwin" "x86_64-linux"];
+      forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
+    in {
+      packages = forEachSystem (system: {
+        default =
+          (inputs.nvf.lib.neovimConfiguration {
+            pkgs = nixpkgs.legacyPackages.${system};
+            modules = [
+              ./configuration.nix
+            ];
+          }).neovim;
+      });
     };
-  };
 }
