c0cdb6f0602d33e71e50ef10933ee006c5c6a395
Woojong Koh <wjngkoh@gmail.com>
2026-01-27 13:50:44 +1000

    feat: use alejandra for formatting and add .envrc

--- flake.nix
+++ flake.nix
@@ -4,31 +4,29 @@
     nvf.url = "github:notashelf/nvf";
   };
 
-  outputs = { nixpkgs, ... } @ inputs:
-    let
-      systems = [ "aarch64-darwin" "x86_64-linux" ];
-      forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
-    in
-    {
-      packages = forEachSystem (system: {
-        default =
-          (inputs.nvf.lib.neovimConfiguration {
-            pkgs = nixpkgs.legacyPackages.${system};
-            modules = [
-              ./configuration.nix
-            ];
-          }).neovim;
-      });
-
-      devShells = forEachSystem (system: {
-        default = nixpkgs.legacyPackages.${system}.mkShell {
-          buildInputs = with nixpkgs.legacyPackages.${system}; [
-            nix
-            gh
+  outputs = {nixpkgs, ...} @ inputs: let
+    systems = ["aarch64-darwin" "x86_64-linux"];
+    forEachSystem = f: nixpkgs.lib.genAttrs systems (system: f system);
+  in {
+    packages = forEachSystem (system: {
+      default =
+        (inputs.nvf.lib.neovimConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system};
+          modules = [
+            ./configuration.nix
           ];
-        };
-      });
+        }).neovim;
+    });
 
-      formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
-    };
+    devShells = forEachSystem (system: {
+      default = nixpkgs.legacyPackages.${system}.mkShell {
+        buildInputs = with nixpkgs.legacyPackages.${system}; [
+          nix
+          gh
+        ];
+      };
+    });
+
+    formatter = forEachSystem (system: nixpkgs.legacyPackages.${system}.alejandra);
+  };
 }
